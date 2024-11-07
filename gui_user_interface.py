388c4964a53572c4bf1c1b3c8a1d2b6d7599a01e
Landon Lamie <landon.m.lamie@gmail.com>
2024-11-07 17:55:15 -0500

    Create GUI file and start working on new beverage collection class.

---
+++ gui_user_interface.py
@@ -0,0 +1,23 @@
+"""Simple Beverage Management GUI"""
+
+# Third-party imports
+import FreeSimpleGUI as sg
+
+# First-party imports
+from errors import AlreadyImportedError
+
+
+class BeverageListWindow:
+    """Simple Beverage List"""
+
+    def __init__(self, beverage_collection, csv_processor, path_to_csv):
+        """Constructor"""
+        self.beverage_collection = beverage_collection
+        self.csv_processor = csv_processor
+        self.path_to_csv = path_to_csv
+
+        layout = [
+            [sg.Text("Beverage List")],
+        ]
+
+        self.window = sg.Window("Beverage List", layout)
